a61da2e8ac30c952d8e59570e92ae9b6e09f81e3
Özcan Kara <ozcankara810@gmail.com>
2023-08-14 23:41:46 +0300

    some changes

---
+++ RealEstate_Dapper_Api/Models/DapperContext/ContextBase.cs
@@ -0,0 +1,11 @@
+﻿using System.Data;
+
+namespace RealEstate_Dapper_Api.Models.DapperContext
+{
+    public class ContextBase
+    {
+        private readonly IConfiguration _configuration;
+        private readonly string _connectionString;
+        public IDbConnection CreateConnection => new SqlConnection
+    }
+}
