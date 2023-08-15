b197989788153e3b49cf21b260db8e402b94c903
Özcan Kara <ozcankara810@gmail.com>
2023-08-15 08:37:44 +0300

    some changes

--- RealEstate_Dapper_Api/Program.cs
+++ RealEstate_Dapper_Api/Program.cs
@@ -1,4 +1,12 @@
+using RealEstate_Dapper_Api.Models.DapperContext;
+using RealEstate_Dapper_Api.Repositories.CategoryRepository;
+
 var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddTransient<Context>();
+builder.Services.AddTransient<ICategoryRepository, CategoryRepository>();
+
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
